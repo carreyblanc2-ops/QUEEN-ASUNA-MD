4131dd34c881913e4ad90aae51a9073fb11b0275
carreyblanc2-ops <carreyblanc2@gmail.com>
2025-10-30 16:12:54 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
-const fs = require('fs');
+const fs = require('fs'); 
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
